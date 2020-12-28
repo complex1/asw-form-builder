a6ca05620beee5fe514d6326f98fa7deb6940198
complex1 <mauryas367@gamil.com>
2020-12-28 21:34:44 +0530

    update lot of thing

--- src/FormBuilder/component/input_text.vue
+++ src/FormBuilder/component/input_text.vue
@@ -2,7 +2,7 @@
   <div class="AswFormBuilder-group">
     <div class="AswFormBuilder-group_label">{{config.label}}<b v-if="config.imp" class="AswFormBuilder-group_imp">*</b></div>
     <div class="AswFormBuilder-group_sublabel">{{config.sublabel}}</div>
-    <input class="AswFormBuilder-group_text AswFormBuilder-group_input" :class="config.block ? 'block' : ''" v-model="config.value" type="text">
+    <input class="AswFormBuilder-group_text AswFormBuilder-group_input" :class="config.block ? 'block' : ''" :placeholder="config.placeholder" v-model="config.value" type="text">
     <div v-if="!!GetMessage" class="AswFormBuilder-group_message " :class="GetMessage.type" v-html="GetMessage.msg" ></div>
   </div>
 </template>
